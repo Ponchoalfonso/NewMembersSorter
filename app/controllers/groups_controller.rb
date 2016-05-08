347e7154e9090d4a4e1cd0f514c535244028bc5c
Alfonso <alfonsitovale@hotmail.com>
2016-05-08 17:52:30 -0500

    Version No funcional
    La accion generar en el controlador groups esta roto

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,21 @@
 class GroupsController < ApplicationController
 
+  #Actions, a partir de aqui solo las acciones del controlador
+
   def index
+  end
+
+  def generate
+    @recommended = Request.where(isRecommended: true)
+    @accepted
+    Request.where(isRecommended: false).order("examMark DESC", "schoolAverage DESC").find_each do |request|
+      @accepted = @accepted+request
+    end
+  end
+
+  #Methods, apartir de aqui solo metodos privados
+  private
+  def sendToGroup()
   end
 
 end
