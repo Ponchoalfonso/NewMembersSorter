66be55d32b4cee86590bf2999330235c9af80d24
Alfonso <alfonsitovale@hotmail.com>
2016-05-08 19:27:48 -0500

    Version no funcional
    Tratamos de mover a cada alumno a su respectivo salon y grupo
    dependiendo de su turno

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,9 @@
 class GroupsController < ApplicationController
 
+  #Variables globales
+  $acceptedM
+  $acceptedV
+
   #Actions, a partir de aqui solo las acciones del controlador
 
   def index
@@ -8,11 +12,65 @@ class GroupsController < ApplicationController
   def generate
     @recommended = Request.where(isRecommended: true)
     @accepted = Request.where(isRecommended: false).order("examMark DESC", "schoolAverage DESC").limit(300)
+    sendToTurn
   end
 
   #Methods, apartir de aqui solo metodos privados
+=begin
   private
-  def sendToGroup()
+  def sendToGroup(accepted)
   end
+=end
+
+  private
+  def sendToTurn
+    turn = true
+    #Turno matutino
+    $acceptedM = @accepted.collect do |student|
+      if turn
+        student
+        turn = false
+      else
+        turn = true
+      end #Termina if
+    end #Termina metodo collect
+
+    #Turno Vespertino
+    $acceptedV = @accepted.collect do |student|
+      if turn
+        turn = false
+      else
+        student
+        turn = true
+      end #Termina if
+    end #Termina metodo collect
+
+  end #Termina metodo sendToTurn
+
+=begin #Este bloque no sirve :()
+  private
+  def isFool(speciality turn)
+    @speciality = ""
+    case speciality
+
+    when 1
+      @speciality = "Pogra"
+    when 2
+      @speciality = "Admi"
+    when 3
+      @speciality = "Electro"
+    when 4
+      @speciality = "Conta"
+    when 5
+      @speciality = "Meca"
+
+    end
+
+    case turn
+    when 0
+      @speciality = "#{@speciality}MA"
+      if()
+  end
+=end
 
 end
