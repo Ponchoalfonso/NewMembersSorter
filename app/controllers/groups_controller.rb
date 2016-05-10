d5237efd5f8e82f6949d78cad69827545f6fb8a6
Alfonso <alfonsitovale@hotmail.com>
2016-05-09 20:43:50 -0500

    Optimizacion al 100%
    La pagina generate carga 6 veces mas rápido

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,12 +6,11 @@ class GroupsController < ApplicationController
   end
 
   def generate
-    @prograMA = Request.where(speciality:1)
     @recommended = Request.where(isRecommended: true)
     @accepted = Request.where(isRecommended: false).order("examMark DESC", "schoolAverage DESC").limit(300)
     #Creo dos variables array que contendran a los alumnos separados
-    @acceptedM = Array.new
-    @acceptedV = Array.new
+    @acceptedM
+    @acceptedV
     @electroMA = Array.new
     @adminMA = Array.new
     @contaMA = Array.new
@@ -41,25 +40,24 @@ class GroupsController < ApplicationController
   def splitIntoTurns(accepted)
     #Mando a los usuarios de mecanica directamente al turno vespertino\
     mecha = accepted.where(speciality: 5)
-    for i in(1..mecha.length)
-      @acceptedV.push(mecha.limit(1).offset(i))
+    @acceptedV = mecha.each do |m|
+      m
     end
 
     #Manda a los alumnos foraneos directamente al turno matutino
     foreign = accepted.where(isForeign: true).where.not(speciality: 5)
-
-    for i in(1..mecha.length)
-      @acceptedM.push(foreign.limit(1).offset(i))
+    @acceptedM = foreign.each do |f|
+      f
     end
 
     #Separamos al resto en dos grupos
     accepted = accepted.where.not(speciality: 5).where(isForeign: false)
-    for i in(1..300)
+    for i in(1..accepted.length)
       if i % 2 == 0
-        @acceptedM.push(accepted.limit(1).offset(i))
+        @acceptedM = @acceptedM + accepted.limit(1).offset(i)
       else
-        @acceptedV.push(accepted.limit(1).offset(i))
-      end#if
-    end #for
+        @acceptedV = @acceptedV + accepted.limit(1).offset(i)
+      end
+    end
   end #splitIntoTurns
 end
